945107de89a2aa58c40e87d9a49dc211026e64c3
Frazer Lillie <frazerlillie@intelligencefusion.co.uk>
2025-03-15 13:15:10 +0000

    early frontend

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\VisitController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
 Route::get('/country', [CountryController::class, 'index']);
 Route::get('/country/{country}', [CountryController::class, 'show']);
 
